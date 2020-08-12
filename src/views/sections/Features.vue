31d847d5410425718246d75adf9b0e620f2e2d6f
Tibor Rogulja <tibor.rogulja@tiskarazagreb.hr>
2020-08-12 10:50:04 +0200

    responsiveness fix

--- src/views/sections/Features.vue
+++ src/views/sections/Features.vue
@@ -3,7 +3,7 @@
     <v-responsive class="mx-auto" max-width="1350">
       <v-container fluid>
         <v-row>
-          <v-col v-for="card in cards" :key="card.title" cols="12" sm="4" md="3">
+          <v-col v-for="card in cards" :key="card.title" cols="12" sm="6" md="3">
             <base-info-card align="center" v-bind="card" />
           </v-col>
         </v-row>
--- src/views/sections/Hero.vue
+++ src/views/sections/Hero.vue
@@ -1,8 +1,8 @@
 <template>
   <section id="hero">
-    <v-img :min-height="minHeight" :height="minHeight" :src="require('@/assets2/floor-plan-on-table-834892.jpg')" class="white--text" gradient="to right, rgba(5, 11, 31, .8), rgba(5, 11, 31, .8)">
+    <v-img :min-height="minHeight" :height="height" :src="require('@/assets2/floor-plan-on-table-834892.jpg')" class="white--text" gradient="to right, rgba(5, 11, 31, .8), rgba(5, 11, 31, .8)">
       <v-container class="fill-height px-4 py-12">
-        <v-responsive class="d-flex align-center" height="100%" max-width="700" width="100%">
+        <v-responsive class="d-flex align-center" :class="{}" height="100%" max-width="700" width="100%">
           <base-heading class="mb-8" title="KVALITETAN MAJSTOR PO VAŠOJ MJERI" />
 
           <base-body class="mb-12">
@@ -27,10 +27,12 @@ export default {
   },
 
   computed: {
+    height() {
+      if (this.$vuetify.breakpoint.mdAndUp) return `calc(100vh - ${this.$vuetify.application.top}px)`;
+      return 'auto';
+    },
     minHeight() {
-      const height = this.$vuetify.breakpoint.mdAndUp ? '100vh' : '50vh';
-
-      return `calc(${height} - ${this.$vuetify.application.top}px)`;
+      return `calc(100vh - ${this.$vuetify.application.top}px)`;
     },
   },
 };
